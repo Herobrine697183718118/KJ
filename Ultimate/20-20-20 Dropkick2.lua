2225bd5ba3ee196c9c60f8c79826685dce0d47ed
Herobrine <dalesisgiorgos@gmail.com>
2024-11-02 16:46:31 +0200

    Update 20-20-20 Dropkick2.lua
    Signed-off-by: Herobrine <dalesisgiorgos@gmail.com>

--- Ultimate/20-20-20 Dropkick2.lua
+++ Ultimate/20-20-20 Dropkick2.lua
@@ -6,3 +6,9 @@ AnimAnim.AnimationId = "rbxassetid://17354976067"
 local Anim = Humanoid:LoadAnimation(AnimAnim)
 AnimAnim.AnimationId = "rbxassetid://0"
 Anim:Play()
+
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://17356346310"
+sound.Parent = workspace
+sound.Volume = 1
+sound:Play()
