0447cefcec366192ccbfe422ff3b3d328d5d11f2
Herobrine <dalesisgiorgos@gmail.com>
2024-11-03 09:33:40 +0200

    Update Swift Sweep.lua
    Signed-off-by: Herobrine <dalesisgiorgos@gmail.com>

--- KJ Basic/Swift Sweep.lua
+++ KJ Basic/Swift Sweep.lua
@@ -100,39 +100,3 @@ local function applyDamageToNearestTarget()
 end
 
 applyDamageToNearestTarget()
-
-
-local preservedNames = {
-    "Summermap",
-    "Camera",
-    "Live",
-    "Terrain",
-    "Map",
-    "Thrown",
-    "Built",
-    "KingCrab"
-}
-
-local function isPreserved(name)
-    for _, preservedName in ipairs(preservedNames) do
-        if name == preservedName then
-            return true
-        end
-    end
-    return false
-end
-
-for _, item in ipairs(workspace:GetChildren()) do
-    if not isPreserved(item.Name) then
-        item:Destroy()
-    end
-end
-
-local terrain = workspace:FindFirstChild("Terrain")
-if terrain then
-    for _, child in ipairs(terrain:GetChildren()) do
-        if child.Name ~= "Clouds" then
-            child:Destroy()
-        end
-    end
-end
